ac97d152d16668bd0c604f8720fc36fd9c510b89
ｘｃｉｔｔｙ２０２９ｘ <chikushinemathew@gmail.com>
2025-07-18 22:55:53 +0530

    a04e: Initial tree skeleton

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ceres.mk
+    $(LOCAL_DIR)/lineage_a04e.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_a04e-user \
+    lineage_a04e-userdebug \
+    lineage_a04e-eng
