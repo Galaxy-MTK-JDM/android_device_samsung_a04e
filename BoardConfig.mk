35d6a4861e336e9898c11ac79809be2ed6c53689
ｘｃｉｔｔｙ２０２９ｘ <chikushinemathew@gmail.com>
2025-07-19 00:20:41 +0530

    a04e: added sepolicy ( clarify )

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,8 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
 include device/mediatek/sepolicy_vndr/SEPolicy.mk
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
 
 # Vendor Security Patch
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
